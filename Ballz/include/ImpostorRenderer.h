ff877c22c05f6a84bde5d2a7b91140303936e75a
Marek Tomco <marek.tomco@eset.sk>
2015-07-10 15:27:36 +0200

    added volumedetection class and impostor renderer class

---
+++ Ballz/include/ImpostorRenderer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "stdafx.h"
+#include "ImpostorTexture.h"
+
+
+class ImpostorRenderer
+{
+public:
+
+	static ImpostorRenderer instance;
+
+	void addGeometry(Ogre::Entity* ent);
+	void clear();
+
+protected:
+
+	ImpostorRenderer() {}
+	~ImpostorRenderer() {}
+
+	std::map<Ogre::Entity*, ImpostorTexture*> textures;
+
+};
---
+++ Ballz/src/ImpostorRenderer.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "ImpostorRenderer.h"
+
+using namespace Ogre;
+
+void ImpostorRenderer::addGeometry(Ogre::Entity* ent)
+{
+	if (textures.find(ent) == textures.end())
+	{
+		ImpostorTexture* txt = new ImpostorTexture();
+		txt->render(ent);
+
+		textures[ent] = txt;
+	}
+}
+
+void ImpostorRenderer::clear()
+{
+	for (auto p : textures)
+	{
+		delete p.second;
+	}
+
+	textures.clear();
+}
+
