8c3d5e1736da8a7bbee11039b1edaa515a0087ed
RazielXT <marektomco@gmail.com>
2016-05-29 22:06:56 +0200

    added 2tex_vc material to new materials folder

--- copyGfxFromSrc.bat
+++ copyGfxFromSrc.bat
@@ -6,6 +6,14 @@ xCOPY %src_folder%\*.hlsl %tar_folder% /D:12-1-2014
 xCOPY %src_folder%\*.program %tar_folder% /D:12-1-2014
 xCOPY %src_folder%\*.material %tar_folder% /D:12-1-2014
 
+SET src_folder=D:\OgreSDK\media\materials
+SET tar_folder=.\materials
+
+xCOPY %src_folder%\*.cg %tar_folder% /D:12-1-2014
+xCOPY %src_folder%\*.hlsl %tar_folder% /D:12-1-2014
+xCOPY %src_folder%\*.program %tar_folder% /D:12-1-2014
+xCOPY %src_folder%\*.material %tar_folder% /D:12-1-2014
+
 SET src_folder=D:\OgreSDK\media\materials\scripts
 SET tar_folder=.\gfxScripts
 
