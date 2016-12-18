7dc06d58276d8bcf312c1ee4ce83c9e2a2fae2f5
RazielXT <marektomco@gmail.com>
2016-12-18 22:10:08 +0100

    started adding grass painting functions

--- Ballz/GrassDensityMap.cpp
+++ Ballz/GrassDensityMap.cpp
@@ -6,6 +6,63 @@ GrassDensityMap::GrassDensityMap()
 {
 }
 
+void GrassDensityMap::applyPaint(float x, float y, float w, float size)
+{
+	/*for (int h = 0; h < rows; h++)
+	{
+		for (int w = 0; w < cols; w++)
+		{
+			data[rows*w + h] += w;
+		}
+	}*/
+
+	if (x < minX || y > maxY || x > maxX || y < minY)
+		return;
+
+	auto& f = read(x, y);
+	f = std::max(0.f,std::min(1.f, f+w));
+}
+
+void GrassDensityMap::fill(float value)
+{
+	for (int i = 0; i < rows*cols; i++)
+	{
+		data[i] = value;
+	}
+}
+
+void GrassDensityMap::scale(float x, float y)
+{
+	float centerX = (minX + maxX) / 2;
+	float centerY = (minY + maxY) / 2;
+
+	float sizeX = (centerX - minX)*x;
+	float sizeY = (centerY - minY)*y;
+
+	allocate(centerX - sizeX, centerX + sizeX, centerY - sizeY, centerY + sizeY);
+}
+
+void GrassDensityMap::allocate(float _minX, float _maxX, float _minY, float _maxY)
+{
+	minX = _minX;
+	maxX = _maxX;
+	minY = _minY;
+	maxY = _maxY;
+
+	rows = ((int)maxX - (int)minX)*pixelsPerUnit;
+	cols = ((int)maxY - (int)minY)*pixelsPerUnit;
+
+	rows = std::max(rows, 1);
+	cols = std::max(cols, 1);
+
+	data = new float[rows*cols];
+}
+
+void GrassDensityMap::preserveOriginal(bool enable)
+{
+	preserve = enable;
+}
+
 void GrassDensityMap::apply(GrassInfo& grass)
 {
 	auto buffer = grass.bake.layer->getColorMap()->getPixelBox();
@@ -23,3 +80,18 @@ void GrassDensityMap::apply(GrassInfo& grass)
 	}
 }
 
+float GrassDensityMap::get(float x, float y)
+{
+	if (x < minX || y > maxY || x > maxX || y < minY)
+		return 0;
+
+	return read(x, y);
+}
+
+float& GrassDensityMap::read(float x, float y)
+{
+	int row = (int)x - (int)minX;
+	int col = (int)y - (int)minY;
+
+	return data[rows*col + row];
+}
--- Ballz/GrassDensityMap.h
+++ Ballz/GrassDensityMap.h
@@ -9,14 +9,23 @@ struct GrassDensityMap
 	void applyPaint(float x, float y, float w, float size);
 
 	void fill(float value);
-	void allocate(float minX, float maxX, float minY, float maxY);
+	void scale(float x, float y);
 
 	void preserveOriginal(bool enable);
 	void apply(GrassInfo& grass);
 
+	float get(float x, float y);
+
 private:
+
+	float& read(float x, float y);
+
+	const int pixelsPerUnit = 2;
+
+	void allocate(float minX, float maxX, float minY, float maxY);
 
 	bool preserve = false;
 	float minX, maxX, minY, maxY;
+	int rows, cols;
 	float* data = nullptr;
 };
